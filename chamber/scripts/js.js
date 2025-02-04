5fc24a0c90529f79317eb82b1414a81b20f640ef
Sabra Crowther <smi17152@byui.edu>
2025-02-03 23:36:57 -0700

    made chamber site responsive for smaller and larger viewports

---
+++ chamber/scripts/js.js
@@ -0,0 +1,19 @@
+
+// Get the last modified date of the document
+const lastModified = document.lastModified;
+
+// Get the placeholder element in the footer
+const lastModifiedElement = document.getElementById('lastModified');
+
+// Insert the last modified date into the placeholder
+lastModifiedElement.textContent = `Last modified: ${lastModified}`;
+
+
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
